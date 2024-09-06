880a56621b71f8274ca60d50d9cc98bad22ec54d
Elijah613 <s12152898@students.duvalschools.org>
2024-09-06 12:01:48 -0400

    v0.1
    Used loops and continue/break statements.

---
+++ 00_computer_science_exercises/02_flow_control_structures/looooooops.py
@@ -0,0 +1,31 @@
+# Looooooops, Elijah Reed, v0.0
+
+# TWO TYPES OF LOOPS
+# for <-- Used when you know how many loops you'll need.
+# while <-- Used when you do not know how many loops you'll need. 
+
+# for loop is like Go Fish, you search each card for what the player asked.
+# while loop is like musical chairs, you move around until the music stops.
+
+# EACH TRIP THROUGH THE ENTIRE LOOP IS CALLED AN iteration
+# "iterate through the list" means use a for loop
+
+# for loops Example -- Iterating a List
+# fruits = ["apple", "banana", "strawberry", "tomato"]
+# for eachFruit in fruits:
+#     print(eachFruit)
+
+# continue keyword -- Skips the current iteration and then finishes the loop.  
+# fruits = ["apple", "banana", "strawberry", "tomato"]
+# for eachFruit in fruits:
+#     if eachFruit == "banana":
+#         continue
+#     print(eachFruit)
+
+# break keyword -- Immediately exit the loop.  
+fruits = ["apple", "banana", "strawberry", "tomato"]
+for eachFruit in fruits:
+    if eachFruit == "banana":
+        break
+    print(eachFruit)
+
