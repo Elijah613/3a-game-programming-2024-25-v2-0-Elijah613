ffb37c8dd60390f2df0777d6f7d04e8a078c3c91
Elijah613 <s12152898@students.duvalschools.org>
2024-09-18 11:51:01 -0400

    RPS v0.1

---
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,14 @@
+# Rock, Paper, Scissors by Elijah Reed, v0.1
+
+# MODULE IMPORTS
+import random
+
+# DATA STRUCTURES -- PLAYER
+playerName = "Test Player"
+playerChoice = None
+playerScore =  0
+
+
+# DATA STRUCTURES -- CPU
+cpuScore = 0
+cpuChoice = None
