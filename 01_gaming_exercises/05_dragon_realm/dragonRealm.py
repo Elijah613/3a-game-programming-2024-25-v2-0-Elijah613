51f86f604cff0edd85b4ca56c44b14f2b92a763e
Elijah613 <s12152898@students.duvalschools.org>
2024-11-26 00:12:07 -0500

    Dragon Realm v0.9

--- 01_gaming_exercises/05_dragon_realm/dragonRealm.py
+++ 01_gaming_exercises/05_dragon_realm/dragonRealm.py
@@ -1,4 +1,4 @@
-# Dragon Realm, Elijah Reed, v0.7
+# Dragon Realm, Elijah Reed, v0.9
 # Awesome job so far!  If you have any finishing touches please add them. 
 
 import random
@@ -18,14 +18,14 @@ saveData = open(logFileName, "a")
 # "w" CREATES FILE, IF FILE EXISTS, ERASE AND OVERWRITE FILE CONTENTS.
 # "a" CREATES FILE, IF FILE EXISTS, APPEND DATA TO THE FILE.
 
-saveData.write("GAME STARTED" + " " + str(datetime.datetime.now()) + "\n")
+saveData.write("\nGAME STARTED" + " " + str(datetime.datetime.now()) + "\n")
 
-evidence = 0
+
 sanity = 100
 items = 0
 hasUSB = False
 hasTea = False
-hasPen = False
+hasLetterOpener = False
 knowsAddress = False
 
 def displayIntro():
@@ -46,7 +46,6 @@ def chooseDoor():
     return doors
 
 def checkRoom(chosenRoom):
-    evidence = 0
     sanity = 100
 
     print('\nYou approach the door...')
@@ -70,9 +69,8 @@ def checkRoom(chosenRoom):
     else:
         print('IT came out of the drawer and grabs you!')
         time.sleep(waitTime)
-        print("You shake IT free but it cut your arm and left some dark substance.\n")
+        print("You shake IT free but it cut your arm.\n")
         time.sleep(waitTime)
-        evidence += 1
         sanity -= 20
         print(f"You have {sanity} sanity left.\n")
         time.sleep(waitTime)
@@ -112,7 +110,7 @@ def roomScenarioComputer(hasUSB: bool):
     time.sleep(waitTime)
     return knowsAddress
 
-def roomScenarioWarehouse(knowsAddress, hasPen, sanity, evidence):
+def roomScenarioWarehouse(knowsAddress, hasLetterOpener, sanity):
     if knowsAddress == True:
         saveData.write("The player went to the warehouse.\n")
         print("You decide to go to that address you found on that old computer.")
@@ -138,17 +136,8 @@ def roomScenarioWarehouse(knowsAddress, hasPen, sanity, evidence):
         print("You snap back to reality, quickly get away before IT attacks you again!")
         time.sleep(waitTime)
         sanity -= 40
-        print("You made it out of the WAREHOUSE, you should draw what it looks like in case you need evidence.")
+        print("You made it out of the WAREHOUSE.")
         time.sleep(waitTime)
-        if hasPen == True:
-            print("You draw IT, this mediocre drawing may come in handy.")
-            time.sleep(waitTime)
-            evidence += 1
-            saveData.write("The player drew the monster.\n")
-        elif hasPen == False:
-            print("You don't have a pen, nevermind.")
-            time.sleep(waitTime)
-            saveData.write("The player didn't draw the monster.\n")
         print("After everything that happened in there, you might as well look around the city.\n")
         time.sleep(waitTime)
 
@@ -157,14 +146,12 @@ def roomScenarioWarehouse(knowsAddress, hasPen, sanity, evidence):
         time.sleep(waitTime)
         saveData.write("The player didn't go to the warehouse.\n")
 
-def roomScenarioCity(sanity, evidence, hasTea):
+def roomScenarioCity(sanity, hasTea):
     if hasTea == True:
         print("Before exploring, you should drink your tea.")
         sanity += 10
     elif hasTea == False:
         print("Only if you had something to drink...")
-
-    trust = 0
 
     time.sleep(waitTime)
     print("While wandering around the city, you find a INFO STAND.")
@@ -184,33 +171,121 @@ def roomScenarioCity(sanity, evidence, hasTea):
     print("??? - I see some dark thing running around but I'm older than I look, my eyes are getting bad.")
     time.sleep(waitTime)
     print("You - Well IT's definetly real! How do I get out of here?")
-    time.sleep(waitTime)
-    print("??? - I would tell you if you weren't crazy.")
-    time.sleep(waitTime)
-    print("You - I'M NOT CRAZY.")
-    time.sleep(waitTime)
-    if sanity >= 50:
-        print("??? - I mean... you seem normal but still..")
-        trust += 5
-    elif sanity < 50:
-        print("??? - Look at you, you're all jittery and twitchy.")
 
-
-    time.sleep(waitTime)
-    print("??? - If this so-called \"void freak\" exists, then I need proof.")
-    time.sleep(waitTime)
-
-    if evidence == 1:
-        trust += 1
-        print("??? - Interesting...")
-    elif evidence == 2:
-        trust += 2
-        print("??? - That's a nasty cut... what is that dark stuff?")
+def checkEnding(sanity):
+    if sanity == 40:
+        print("??? - Have you seen yourself?! You look insane!")
         time.sleep(waitTime)
-        print("??? - That drawing... I have seen that running around.")
+        print("??? - Get out of my sight.")
+        time.sleep(waitTime)
+        print("He won't let you leave, however you aren't satisfied with that.")
+        time.sleep(waitTime)
+        print("You - LET ME OUT OF THIS PRISON!")
+        time.sleep(waitTime)
+        print("IT heard your screams.")
+        time.sleep(waitTime)
+        print("??? - Stop your complaining, it's not that ba-")
+        time.sleep(waitTime)
+        print("IT killed him, and IT wants you next!")
+        print("- The Worst Ending -")
+
+    elif sanity == 50:
+        print("??? - I can see it in your eyes, you can't be ok in the head.")
+        time.sleep(waitTime)
+        print("??? - I'm sorry but I can't tell you how to leave.")
+        time.sleep(waitTime)
+        print("You must just survive in this dimension.")
+        time.sleep(waitTime)
+        print("You live out the rest of your days fending IT off.")
+        print("- The Survival Ending -")
+
+    elif sanity == 60:
+        print("??? - I can tell you've been through alot, so I'll tell you.")
+        time.sleep(waitTime)
+        print("??? - Follow these instructions to the letter.")
+        time.sleep(waitTime)
+        print("The mysterious man tells you how to escape...")
+        time.sleep(waitTime)
+        print("After finding the way out, you wake up!")
+        time.sleep(waitTime)
+        print("You're in a hospital laying in bed!")
+        time.sleep(waitTime)
+        print("You were in a coma this whole time?!")
+        time.sleep(waitTime)
+        print("You - THIS CAN'T BE!")
+        time.sleep(waitTime)
+        print("Relax, you're going insane!")
+        time.sleep(waitTime)
+        print("You - IT WAS REAL, IT WAS ALL REAL!")
+        time.sleep(waitTime)
+        print("Doctor - Sir, calm down, anything you saw wasn't real!")
+        time.sleep(waitTime)
+        print("You - AAAAAHHHHHHH")
+        time.sleep(waitTime)
+        print("You went insane.")
+        print("- The Insane Ending -")
+
+    elif sanity == 70:
+        print("You seem trustworthy... fine, I'll tell you.")
+        time.sleep(waitTime)
+        print("After you follow his instructions, you find a strange door.")
+        time.sleep(waitTime)
+        print("Upon entering, you wake up!")
+        time.sleep(waitTime)
+        print("It was all a big nightmare, you were in a coma.")
+        time.sleep(waitTime)
+        print("You wake up in a hospital, the doctors are suprised to see your conscienceness.")
+        time.sleep(waitTime)
+        print("You have a stabbing headache and your brain feels like it's on fire.")
+        time.sleep(waitTime)
+        print("The doctors take you to Intensive Care.")
+        time.sleep(waitTime)
+        print("You live out the rest of your days in and out the hospital due to your head, it could be worse.")
+        print("- The Mentally Ill ending -")
+
+    elif sanity == 80:
+        print("After you follow his instructions, you find a strange door.")
+        time.sleep(waitTime)
+        print("Upon entering, you wake up!")
+        time.sleep(waitTime)
+        print("It was all a big nightmare, you were in a coma.")
+        time.sleep(waitTime)
+        print("You wake up in a hospital, the doctors are suprised to see your conscienceness.")
+        time.sleep(waitTime)
+        print("As the doctors tried to explain what happened, everything you saw is so... confusing.")
+        time.sleep(waitTime)
+        print("You can't even hear their voices, you just keep thinking about IT.")
+        time.sleep(waitTime)
+        print("IT was so strange, yet you feel a connection to it.")
+        print("- The Confused Ending -")
+
+    elif sanity >= 90:
+        print("??? - Yea I can tell, you are determined to leave, without a trace of rage, sadness, or doubt.")
+        time.sleep(waitTime)
+        print("??? - Here's how you leave.")
+        time.sleep(waitTime)
+        print("Upon following the instructions, you find a strange door.")
+        time.sleep(waitTime)
+        print("When you enter, you wake up! You were in a coma.")
+        time.sleep(waitTime)
+        print("The doctors are suprised to see you wake up and you are so relieved you made it out.")
+        time.sleep(waitTime)
+        print("After brain analysis, you haven't changed.")
+        time.sleep(waitTime)
+        print("You tell the doctors everything you saw and experienced without sparing any details.")
+        time.sleep(waitTime)
+        print("They explain to you and your wife how after a severe car accident after leaving your OFFICE job,")
+        print("you were sent in a coma for 3 years and your wife passed.")
+        time.sleep(waitTime)
+        print("It makes sense, the OFFICE is were you worked.")
+        time.sleep(waitTime)
+        print("The CITY is the last thing you saw before you crashed.")
+        time.sleep(waitTime)
+        print("Whatever was chasing you down didn't want you to leave.")
+        time.sleep(waitTime)
+        print("Everything makes sense now, that thing... IT was your wife.")
+        print("- The Best Ending -")
     
-
-
 playAgain = 'yes'
 
 while playAgain == 'yes' or playAgain == 'y':
@@ -222,9 +297,9 @@ while playAgain == 'yes' or playAgain == 'y':
     checkRoom(roomNumber)
     while items != 2:
         print('Take an item.')
-        itemChoice = input('The [P]en and paper, the [U]SB, or the [T]ea on the desk.\n').upper()
-        if itemChoice == "P":
-            hasPen = True
+        itemChoice = input('The [L]etter Opener, the [U]SB, or the [T]ea on the desk.\n').upper()
+        if itemChoice == "L":
+            hasLetterOpener = True
             print("Write down your feelings with this glorious pen, I'm sure that will help you.")
         elif itemChoice == "U":
             hasUSB = True
@@ -236,8 +311,8 @@ while playAgain == 'yes' or playAgain == 'y':
 
     print("You grabbed: ")
     if hasPen:
-        print("- A pen.")
-        saveData.write("The player chose the pen.\n")
+        print("- A Letter Opener.")
+        saveData.write("The player chose the Letter Opener.\n")
     if hasUSB:
         print("- The USB.")
         saveData.write("The player chose the USB.\n")
@@ -247,8 +322,9 @@ while playAgain == 'yes' or playAgain == 'y':
 
     print('You have your garbage, now proceed.\n')
     knowsAddress = roomScenarioComputer(hasUSB)
-    roomScenarioWarehouse(knowsAddress, hasPen, sanity, evidence)
-    roomScenarioCity(sanity, evidence, hasTea)
+    roomScenarioWarehouse(knowsAddress, hasPen, sanity)
+    roomScenarioCity(sanity, hasTea)
+    checkEnding(sanity)
     print('Do you want to play again? (yes or no)\n')
     playAgain = input()
 
